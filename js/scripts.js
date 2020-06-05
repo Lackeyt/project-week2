00ad894c1d3edfdba666d9b1af1bf4f745e634c6
Tyson Lackey <lackeyt90@gmail.com>
2020-06-05 12:17:04 -0700

    update checked logic for multi-select

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,30 @@ $(document).ready(function() {
     const firstName = $("input#q1").val();
     const lastName = $("input#q2").val();
     const experience = $("#q3").val();
-    //const swift = $("#swift").is("checked");
-    //const java = $("#java").is("checked");
-    //const html = $("#html").is("checked");
-    //const python = $("#python").is("checked");
-    //const r = $("#r").is("checked");
-    //const c = $("#c").is("checked");
+    const swift = $("input#swift:checked").val();
+    const java = $("input#java:checked").val();
+    const koltin = $("input#koltin:checked").val();
+    const html = $("input#html:checked").val();
+    const python = $("input#python:checked").val();
+    const r = $("input#r:checked").val();
+    const c = $("input#c:checked").val();
     const industry = $("#q5").val();
 
-    return firstName
+    let langRec
+    if (industry === "iOS") {
+      langRec = "Swift"
+    } else if (industry === "android") {
+      if (java === "1" && koltin === false){
+        langRec = "Koltin"
+      } else if (koltin === true && java === false) {
+        langRec = "Java"
+      } else if (koltin === false && java === false) {
+        langRec = "Java or Koltin"
+      } else if (koltin === true && java === true) {
+        langRec = "More Java or Koltin"
+      };
+    } 
+    
+    $(".language").text(langRec)
   });
 });
