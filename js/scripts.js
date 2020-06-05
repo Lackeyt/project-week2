a2356ba55d1853c169122116e4dff3ae9d43612a
Tyson Lackey <lackeyt90@gmail.com>
2020-06-05 13:21:47 -0700

    add form input validation

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,35 @@ $(document).ready(function() {
     let langRec
     let why
     const fullName = firstName + " " + lastName + ","
+    let rq1v = true
+    let rq2v = true
+    let rq3v = true
+    let rq5v = true
+
+    if (firstName === "") {
+      rq1v = false
+    }
+
+    if (lastName === "") {
+      rq2v = false
+    }
+
+    if (experience === null) {
+      rq3v = false
+    }
 
     if (industry === "iOS") {
+      if (swift) {
+        langRec = "continuing with Swift"
+        why = "because it's the primary language of iOS development"
+      } else {
       langRec = "Swift"
+      why = "because it's the primary language of iOS development"
+      }
     } else if (industry === "android") {
       if (koltin === "1" && java === "1"){
-        langRec = "More Java or Koltin"
+        langRec = "more Java or Koltin"
+        why = "because they're the primary languages in Android development"
       } else if (koltin === "1") {
         langRec = "Java"
       } else if (java === "1") {
@@ -43,10 +66,46 @@ $(document).ready(function() {
     } else if (industry === "other") {
         langRec = "Python";
         why = " because it is forgiving and one of the fastest growing languages";
+    } else {
+      rq5v = false
     };
     
-    $(".name").text(fullName);
-    $(".language").text(langRec);
-    $(".why").text(why);
+    if (rq1v === false || rq2v === false || rq3v === false || rq5v === false) {
+      if (rq1v === false) {
+        $("li.rq1").show()
+        $("input#q1").addClass("emptyForm")
+      } else {
+        $("li.rq1").hide()
+        $("input#q1").removeClass("emptyForm")
+      }
+      if (rq2v === false) {
+        $("li.rq2").show()
+        $("input#q2").addClass("emptyForm")
+      } else {
+        $("li.rq2").hide()
+        $("input#q2").removeClass("emptyForm")
+      }
+      if (rq3v === false) {
+        $("li.rq3").show()
+      } else {
+        $("li.rq3").hide()
+      }
+      if (rq5v === false) {
+        $("li.rq5").show()
+      } else {
+        $("li.rq5").hide()
+      }
+    } else {
+      $(".output").show()
+      $(".name").text(fullName);
+      $(".language").text(langRec);
+      $(".why").text(why);
+      $("li.rq5").hide()
+      $("li.rq3").hide()
+      $("li.rq2").hide()
+      $("input#q2").removeClass("emptyForm")
+      $("li.rq1").hide()
+      $("input#q1").removeClass("emptyForm")
+    }
   });
 });
